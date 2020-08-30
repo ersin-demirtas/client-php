8e6bc127f70ef1b0235ca0b950ae9087f7198501
Ersin Demirtas <ed5@me.com>
2020-08-30 18:50:03 +0100

    removed code dublication

--- src/Rest/Stocks/HistoricQuotes.php
+++ src/Rest/Stocks/HistoricQuotes.php
@@ -1,33 +1,46 @@
 <?php
+
 namespace PolygonIO\Rest\Stocks;
 
+use PolygonIO\Rest\Common\Mappers;
 use PolygonIO\Rest\RestResource;
 
+/**
+ * Class HistoricQuotes
+ *
+ * @package PolygonIO\Rest\Stocks
+ */
 class HistoricQuotes extends RestResource {
 
-    protected array $defaultParams
-        = [
-            'limit' => 100
-        ];
+    /**
+     * @var array|int[]
+     */
+    protected array $defaultParams = [
+        'limit' => 100
+    ];
 
+    /**
+     * @param $tickerSymbol
+     * @param $date
+     *
+     * @return array|mixed
+     */
     public function get($tickerSymbol, $date)
     {
         return $this->_get('/v1/historic/quotes/'.$tickerSymbol.'/'.$date);
     }
 
+    /**
+     * @param  array  $response
+     *
+     * @return array
+     */
     protected function mapper(array $response): array
     {
-        $response['ticks'] = array_map(function ($tick) {
-            $tick['condition'] = $tick['c'];
-            $tick['bidExchange'] = $tick['bE'];
-            $tick['askExchange'] = $tick['aE'];
-            $tick['askPrice'] = $tick['aP'];
-            $tick['buyPrice'] = $tick['bP'];
-            $tick['bidSize'] = $tick['bS'];
-            $tick['askSize'] = $tick['aS'];
-            $tick['timestamp'] = $tick['t'];
-            return $tick;
+        $response['ticks'] = array_map(function ($result) {
+            return Mappers::quoteV1($result);
         }, $response['ticks']);
+
         return $response;
     }
 }
--- src/Rest/Stocks/HistoricTrades.php
+++ src/Rest/Stocks/HistoricTrades.php
@@ -1,33 +1,45 @@
 <?php
 namespace PolygonIO\Rest\Stocks;
 
+use PolygonIO\Rest\Common\Mappers;
 use PolygonIO\Rest\RestResource;
 
+/**
+ * Class HistoricTrades
+ *
+ * @package PolygonIO\Rest\Stocks
+ */
 class HistoricTrades extends RestResource {
 
-    protected array $defaultParams
-        = [
-            'limit' => 100
-        ];
+    /**
+     * @var array|int[]
+     */
+    protected array $defaultParams = [
+        'limit' => 100
+    ];
 
+    /**
+     * @param $tickerSymbol
+     * @param $date
+     *
+     * @return array|mixed
+     */
     public function get($tickerSymbol, $date)
     {
         return $this->_get('/v1/historic/trades/'.$tickerSymbol.'/'.$date);
     }
 
+    /**
+     * @param  array  $response
+     *
+     * @return array
+     */
     protected function mapper(array $response): array
     {
-        $response['ticks'] = array_map(function ($tick) {
-            $tick['condition1'] = $tick['c1'];
-            $tick['condition2'] = $tick['c2'];
-            $tick['condition3'] = $tick['c3'];
-            $tick['condition4'] = $tick['c4'];
-            $tick['exchange'] = $tick['e'];
-            $tick['price'] = $tick['p'];
-            $tick['size'] = $tick['s'];
-            $tick['timestamp'] = $tick['t'];
-            return $tick;
+        $response['ticks'] = array_map(function ($result) {
+            return Mappers::tradeV1($result);
         }, $response['ticks']);
+
         return $response;
     }
 }
